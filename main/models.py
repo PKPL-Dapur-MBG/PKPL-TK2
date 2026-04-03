8866208cb98a87b323651bdc3bf2f621ecb7e279
rexy <rexy.adrian.f@gmail.com>
2026-04-03 20:10:05 +0700

    fix main color edit feature

--- main/models.py
+++ main/models.py
@@ -3,6 +3,8 @@ from django.db import models
 class WebStyle(models.Model):
     bg_color = models.CharField(max_length=20, default="#f3f4f6") 
     
+    text_color = models.CharField(max_length=20, default="#1f2937")
+
     secondary_color = models.CharField(max_length=20, default="#4f46e5")
 
     btn_text_color = models.CharField(max_length=20, default="#ffffff")
